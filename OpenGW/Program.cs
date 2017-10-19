802f7c1f56e6df0731825d580640c9548f99044f
OpenGW <opengw@fastmail.com>
2017-10-19 22:11:04 +0800

    Create OpenGW solution and project (.NET Core 2.0)

---
+++ OpenGW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenGW
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
